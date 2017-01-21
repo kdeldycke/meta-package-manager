32f35a406a3cb5dd60b0dcd3da6287fe4db19032
Kevin Deldycke <kevin@deldycke.com>
2017-01-21 16:33:54 -0400

    Use interactive setup.py CLI to fetch package metadata.
    Keep the same ordering of methods as the source.
    Remove unused Sphinx extension.

--- docs/conf.py
+++ docs/conf.py
@@ -8,46 +8,54 @@ from __future__ import (
 )
 
 import os
+import subprocess
 import sys
 import time
 
-# Expose package to autodoc.
-sys.path.insert(0, os.path.abspath('..'))
-import meta_package_manager  # noqa  # isort:skip
 
+# Fetch general information about the project.
+# Source: https://github.com/jaraco/skeleton/blob/skeleton/docs/conf.py
+root = os.path.join(os.path.dirname(__file__), '..')
+setup_script = os.path.join(root, 'setup.py')
+fields = ['--name', '--version', '--url', '--author']
+dist_info_cmd = [sys.executable, setup_script] + fields
+output_bytes = subprocess.check_output(dist_info_cmd, cwd=root)
+project_id, version, url, author = output_bytes.decode(
+    'utf-8').strip().split('\n')
+
+# Title-case each word of the project ID.
+project = ' '.join([word.title() for word in project_id.split('-')])
+htmlhelp_basename = project_id
+
+release = version
+
+# Addons.
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
     'sphinx.ext.todo',
-    'sphinx.ext.viewcode',
-]
+    'sphinx.ext.viewcode']
 
 master_doc = 'index'
-
-project = "Meta Package Manager"
-author = "Kevin Deldycke"
 
 # We use our own copyright template instead of the default as the latter strip
 # HTML content.
 html_show_copyright = False
 copyright = (
-    "2016-{}, <a href='https://kevin.deldycke.com'>{}</a> and "
-    "<a href='https://github.com/kdeldycke/meta-package-manager/graphs/"
-    "contributors'>contributors</a>").format(time.strftime('%Y'), author)
-
-version = release = meta_package_manager.__version__
+    "2016-{}, <a href='https://kevin.deldycke.com'>{}</a> and <a href='https:"
+    "//github.com/kdeldycke/{}/graphs/contributors'>contributors</a>").format(
+        time.strftime('%Y'), author, project_id)
 
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 nitpicky = True
+
 # We need a recent sphinx because of the last update format.
 needs_sphinx = '1.4'
 html_last_updated_fmt = 'YYYY-MM-dd'
 templates_path = ['templates']
 
-# Enforce Google style docstrings.
-napoleon_google_docstring = True
-napoleon_numpy_docstring = False
+# Keep the same ordering as in original source code.
+autodoc_member_order = 'bysource'
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = True
@@ -59,5 +67,3 @@ if not on_rtd:
     import sphinx_rtd_theme
     html_theme = 'sphinx_rtd_theme'
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-
-htmlhelp_basename = project

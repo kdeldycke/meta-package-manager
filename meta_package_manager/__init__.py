bde92e3a96769f0d47f40a38219163898fc0273f
Kevin Deldycke <kevin@deldycke.com>
2017-01-21 16:05:46 -0400

    Add version docstring.

--- meta_package_manager/__init__.py
+++ meta_package_manager/__init__.py
@@ -33,6 +33,17 @@ from .bitbar import fix_environment
 
 
 __version__ = '2.4.0'
+""" Examples of valid version strings according `PEP-440
+<https://www.python.org/dev/peps/pep-0440/#version-scheme>`_:
+
+.. code-block:: python
+    __version__ = '1.2.3.dev1'   # Development release 1
+    __version__ = '1.2.3a1'      # Alpha Release 1
+    __version__ = '1.2.3b1'      # Beta Release 1
+    __version__ = '1.2.3rc1'     # RC Release 1
+    __version__ = '1.2.3'        # Final Release
+    __version__ = '1.2.3.post1'  # Post Release 1
+"""
 
 
 logger = logging.getLogger(__name__)

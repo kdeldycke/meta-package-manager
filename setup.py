1863a4457892a052aa53c1e2a2ae08bf3f7814c4
Kevin Deldycke <kevin@deldycke.com>
2017-02-20 01:24:25 +0100

    Beautify.

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ DEPENDENCIES = [
     # shutil.which() is only available starting with Python 3.3. Use backports
     # for Python 2. See marker usage at: https://setuptools.readthedocs.io/en
     # /latest/setuptools.html#declaring-platform-specific-dependencies
-    'backports.shutil_which>=3.5.1;python_version<"3.3"',
+    'backports.shutil_which >= 3.5.1 ; python_version < "3.3"',
 ]
 
 EXTRA_DEPENDENCIES = {

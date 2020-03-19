b920c8c0e9bd98eac080b6023de6863c10bb66e4
Kevin Deldycke <kevin@deldycke.com>
2020-03-19 11:11:13 +0100

    Tweak CLI output to fit 80 char limit.

--- meta_package_manager/managers/pip.py
+++ meta_package_manager/managers/pip.py
@@ -45,7 +45,7 @@ class Pip(PackageManager):
         .. code-block:: shell-session
 
             $ pip --version
-            pip 20.0.2 from /usr/local/lib/python3.7/site-packages/pip (python 3.7)
+            pip 2.0.2 from /usr/local/lib/python/site-packages/pip (python 3.7)
         """
         output = self.run([self.cli_path, '--version'])
         if output:

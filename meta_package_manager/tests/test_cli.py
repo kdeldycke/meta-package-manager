c3b0707f6ec67cd766272f9c57e3dd5007076a6e
Kevin Deldycke <kevin@deldycke.com>
2020-03-22 18:08:57 +0100

    Unittest restore command.

--- meta_package_manager/tests/test_cli.py
+++ meta_package_manager/tests/test_cli.py
@@ -18,6 +18,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
+import textwrap
 import unittest
 
 import simplejson as json
@@ -506,3 +507,65 @@ class TestCLIBackup(TestCLISubcommand):
             self.assertSetEqual(
                 {l for l in doc.read().split() if l.startswith('[')},
                 set(['[npm]']))
+
+
+class TestCLIRestore(TestCLISubcommand):
+
+    subcommand_args = ['restore', 'dummy.toml']
+
+    def setUp(self, *args, **kwargs):
+        """ Create a custom TOML file to feed the CLI with.
+
+        Our dummy file should result in no action as whole sections of
+        unrecognized managers are simply ignored.
+        """
+        toml_content = textwrap.dedent("""\
+            [dummy_manager]
+            fancy_package = "0.0.1"
+            """)
+
+        with open('dummy.toml', 'w') as doc:
+            doc.write(toml_content)
+
+        super(TestCLIRestore, self).setUp(*args, **kwargs)
+
+    def test_ignore_dummy_manager(self):
+        result = self.invoke('restore', 'dummy.toml')
+        self.assertEqual(result.exit_code, 0)
+        self.assertIn('dummy.toml', result.output)
+
+    def test_restore_single_manager(self):
+        toml_content = textwrap.dedent("""\
+            [apm]
+            fancy_package = "0.0.1"
+
+            [npm]
+            dummy_package = "2.2.2"
+            """)
+
+        with open('packages.toml', 'w') as doc:
+            doc.write(toml_content)
+
+        result = self.invoke('--manager', 'npm', 'restore', 'packages.toml')
+        self.assertEqual(result.exit_code, 0)
+        self.assertIn('packages.toml', result.output)
+        self.assertNotIn('Restore apm', result.output)
+        self.assertIn('Restore npm', result.output)
+
+    def test_restore_excluded_manager(self):
+        toml_content = textwrap.dedent("""\
+            [apm]
+            fancy_package = "0.0.1"
+
+            [npm]
+            dummy_package = "2.2.2"
+            """)
+
+        with open('packages.toml', 'w') as doc:
+            doc.write(toml_content)
+
+        result = self.invoke('--exclude', 'npm', 'restore', 'packages.toml')
+        self.assertEqual(result.exit_code, 0)
+        self.assertIn('packages.toml', result.output)
+        self.assertIn('Restore apm', result.output)
+        self.assertNotIn('Restore npm', result.output)

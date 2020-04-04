33d93ddbf60b3f056f1dac2cf7ebca1bb12ede9c
Kevin Deldycke <kevin@deldycke.com>
2020-04-04 10:53:09 +0200

    Next release no longer bugfix only. Bump revision.

--- meta_package_manager/__init__.py
+++ meta_package_manager/__init__.py
@@ -23,7 +23,7 @@ import logging
 
 from .bitbar import fix_environment
 
-__version__ = '3.1.1'
+__version__ = '3.2.0'
 """ Examples of valid version strings according :pep:`440#version-scheme`:
 
 .. code-block:: python
--- meta_package_manager/bitbar/meta_package_manager.7h.py
+++ meta_package_manager/bitbar/meta_package_manager.7h.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # <bitbar.title>Meta Package Manager</bitbar.title>
-# <bitbar.version>v3.1.1</bitbar.version>
+# <bitbar.version>v3.2.0</bitbar.version>
 # <bitbar.author>Kevin Deldycke</bitbar.author>
 # <bitbar.author.github>kdeldycke</bitbar.author.github>
 # <bitbar.desc>List outdated packages and manage upgrades.</bitbar.desc>

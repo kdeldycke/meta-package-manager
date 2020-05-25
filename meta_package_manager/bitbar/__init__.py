abf89e79aac9899e64c8f27e05aaa342cd4156b7
Kevin Deldycke <kevin@deldycke.com>
2020-05-25 23:22:59 +0200

    Add support for Python 2 to have unittests running.

--- meta_package_manager/bitbar/__init__.py
+++ meta_package_manager/bitbar/__init__.py
@@ -17,13 +17,29 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-from importlib.machinery import SourceFileLoader
-from pathlib import Path
+""" This is the last place in which we support Python 2.7 to allow BitBar
+plugin to be tested out in our CI workflow.
+"""
+
+try:
+    # Python 3.
+    from importlib.machinery import SourceFileLoader
+    from pathlib import Path
+    PY = 3
+except ImportError:
+    # Python 2.
+    import imp
+    from os import path
+    PY = 2
+
 
 # Manually import BitBar plugin source code as a module, because of its non
 # Python-compliant filename with a double extension made the first dot
 # interpreted as a submodule.
-SourceFileLoader(
-    __name__,
-    str(Path(__file__).parent.joinpath('meta_package_manager.7h.py').resolve())
-).load_module()
+if PY == 2:
+    here = path.dirname(path.abspath(__file__))
+    bitbar_plugin = path.join(here, 'meta_package_manager.7h.py')
+    imp.load_source(__name__, bitbar_plugin)
+else:
+    SourceFileLoader(__name__, str(Path(__file__).parent.joinpath(
+        'meta_package_manager.7h.py').resolve())).load_module()

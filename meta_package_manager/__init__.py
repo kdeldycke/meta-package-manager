b5005d5b3caef0a439457ea6d6a682c925123e38
Kevin Deldycke <kevin@deldycke.com>
2020-08-18 11:16:30 +0200

    Next release no longer bug-fix only. Bump version.

--- meta_package_manager/__init__.py
+++ meta_package_manager/__init__.py
@@ -26,7 +26,7 @@ from .bitbar import fix_environment
 # Canonical name of the CLI.
 CLI_NAME = "mpm"
 
-__version__ = "3.3.1"
+__version__ = "3.4.0"
 """ Examples of valid version strings according :pep:`440#version-scheme`:
 
 .. code-block:: python

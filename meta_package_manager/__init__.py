ce19f1247136592d2caff0e85cc189cac4ff6c7f
Kevin Deldycke <kevin@deldycke.com>
2021-10-01 14:32:56 +0400

    Next release no longer bug-fix only. Bump revision.

--- meta_package_manager/__init__.py
+++ meta_package_manager/__init__.py
@@ -23,7 +23,7 @@ from boltons.ecoutils import get_profile
 
 from .xbar import fix_environment
 
-__version__ = "4.5.1"
+__version__ = "4.6.0"
 """ Examples of valid version strings according :pep:`440#version-scheme`:
 
 .. code-block:: python

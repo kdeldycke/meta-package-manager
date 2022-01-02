7f5dace236a03c07939f244f3110a7b5730bcb60
Kevin Deldycke <kevin@deldycke.com>
2022-01-02 17:00:42 +0100

    Typo.

--- meta_package_manager/pool.py
+++ meta_package_manager/pool.py
@@ -15,7 +15,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-""" Registration, indexing and cache of package manager definitions. """
+""" Registration, indexing and caching of package manager definitions. """
 
 import inspect
 from importlib import import_module

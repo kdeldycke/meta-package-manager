bc4f60cb17270294c60ce566fad74f94747a3f6d
Kevin Deldycke <kevin@deldycke.com>
2022-04-13 13:27:48 +0400

    Remove unused import.

--- meta_package_manager/tests/test_managers.py
+++ meta_package_manager/tests/test_managers.py
@@ -16,7 +16,6 @@
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 import ast
-import inspect
 import re
 from operator import attrgetter
 from pathlib import Path, PurePath

de2fb1d9023ea143db4efef6dbf99804d4a730a1
kdeldycke <kdeldycke@users.noreply.github.com>
2022-11-14 15:09:23 +0000

    [changelog] Bump minor version to v5.9.0.

--- meta_package_manager/__init__.py
+++ meta_package_manager/__init__.py
@@ -17,7 +17,7 @@
 
 """Expose package-wide elements."""
 
-__version__ = "5.8.1"
+__version__ = "5.9.0"
 """ Examples of valid version strings according :pep:`440#version-scheme`:
 
 .. code-block:: python

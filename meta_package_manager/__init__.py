f9f813b92b72f8325bae3ad4cd3169dd7f1b6ec5
kdeldycke <kdeldycke@users.noreply.github.com>
2022-11-17 12:55:20 +0000

    [changelog] Post-release version bump

--- meta_package_manager/__init__.py
+++ meta_package_manager/__init__.py
@@ -17,7 +17,7 @@
 
 """Expose package-wide elements."""
 
-__version__ = "5.9.0"
+__version__ = "5.9.1"
 """ Examples of valid version strings according :pep:`440#version-scheme`:
 
 .. code-block:: python

acc8e4a07ff4dadd8b45736fe1dffb7b105ce526
kdeldycke <kdeldycke@users.noreply.github.com>
2023-06-16 12:00:43 +0000

    [changelog] Bump minor version to v5.14.0

--- meta_package_manager/__init__.py
+++ meta_package_manager/__init__.py
@@ -15,7 +15,7 @@
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 """Expose package-wide elements."""
 
-__version__ = "5.13.2"
+__version__ = "5.14.0"
 """Examples of valid version strings according :pep:`440#version-scheme`:
 
 .. code-block:: python

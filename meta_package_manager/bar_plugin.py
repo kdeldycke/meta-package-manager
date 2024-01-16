8defef61fc9327daecd367cf00aba548d6ac5459
kdeldycke <kdeldycke@users.noreply.github.com>
2024-01-16 10:43:20 +0000

    [changelog] Post-release version bump

--- meta_package_manager/bar_plugin.py
+++ meta_package_manager/bar_plugin.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # <xbar.title>Meta Package Manager</xbar.title>
-# <xbar.version>v5.14.1</xbar.version>
+# <xbar.version>v5.14.2</xbar.version>
 # <xbar.author>Kevin Deldycke</xbar.author>
 # <xbar.author.github>kdeldycke</xbar.author.github>
 # <xbar.desc>List outdated packages and manage upgrades.</xbar.desc>

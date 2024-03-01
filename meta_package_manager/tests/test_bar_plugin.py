38a38a96a8cfef95bc129e077ad30e70bfbed460
Kevin Deldycke <kevin@deldycke.com>
2024-03-01 11:34:22 +0400

    Try something else

--- meta_package_manager/tests/test_bar_plugin.py
+++ meta_package_manager/tests/test_bar_plugin.py
@@ -23,7 +23,7 @@ from itertools import product
 
 import pytest
 from boltons.iterutils import flatten
-from click_extra.testing import args_cleanup, env_copy
+from click_extra.testing import args_cleanup
 from click_extra.tests.conftest import unless_macos
 
 from meta_package_manager import bar_plugin
@@ -170,10 +170,10 @@ class TestBarPlugin:
     def plugin_output_checks(self, checklist, extra_env=None):
         """Run the plugin script and check its output against the checklist."""
         process = subprocess.run(
-            _subcmd_args(None, bar_plugin.__file__),
+            bar_plugin.__file__,
             capture_output=True,
             encoding="utf-8",
-            env=env_copy(extra_env),
+            env=extra_env,
         )
 
         assert not process.stderr

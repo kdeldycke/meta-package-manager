16ff1a6cb8e7ed8ad34f4ffa8a9c0385e8d359ac
Kevin Deldycke <oss@deldycke.net>
2024-10-27 19:15:36 +0400

    Fix manager count

--- tests/test_pool.py
+++ tests/test_pool.py
@@ -64,8 +64,8 @@ def test_manager_classes_order():
 
 def test_manager_count():
     """Check all implemented package managers are accounted for, and unique."""
-    assert len(manager_classes) == 31
-    assert len(pool) == 31
+    assert len(manager_classes) == 32
+    assert len(pool) == 32
     assert len(pool) == len(pool.all_manager_ids)
     assert pool.all_manager_ids == tuple(sorted(set(pool)))
 

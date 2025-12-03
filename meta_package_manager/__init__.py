cc88c40dd2dd7557672492a935caee4d68456f35
Kevin Deldycke <kevin@deldycke.com>
2025-12-03 11:24:02 +0400

    Remove verbose doc

--- meta_package_manager/__init__.py
+++ meta_package_manager/__init__.py
@@ -18,14 +18,3 @@
 from __future__ import annotations
 
 __version__ = "5.21.1"
-"""Examples of valid version strings according :pep:`440#version-scheme`:
-
-.. code-block:: python
-
-    __version__ = "1.2.3.dev1"  # Development release 1
-    __version__ = "1.2.3a1"  # Alpha Release 1
-    __version__ = "1.2.3b1"  # Beta Release 1
-    __version__ = "1.2.3rc1"  # RC Release 1
-    __version__ = "1.2.3"  # Final Release
-    __version__ = "1.2.3.post1"  # Post Release 1
-"""

c49c4a5f623ee2d72ccdf6eb4563c2e2477cf7fb
Kevin Deldycke <kevin@deldycke.com>
2026-03-23 09:06:12 +0100

    Migrate auto-generated docs code to `docs_update.py`

--- meta_package_manager/inventory.py
+++ meta_package_manager/inventory.py
@@ -17,10 +17,6 @@
 
 from __future__ import annotations
 
-from pathlib import Path
-from textwrap import dedent
-
-from click_extra.table import TableFormat, render_table
 from extra_platforms import (
     ALL_WINDOWS,
     BSD_WITHOUT_MACOS,
@@ -28,10 +24,7 @@ from extra_platforms import (
     MACOS,
     UNIX_WITHOUT_MACOS,
     Group,
-    extract_members,
 )
-from .base import Operations
-from .pool import pool
 
 TYPE_CHECKING = False
 if TYPE_CHECKING:
@@ -59,146 +52,3 @@ make it readable both in CLI and documentation.
 
 The order of this list determine the order of the resulting columns.
 """
-
-
-def managers_sankey() -> str:
-    """Produce a sankey diagram to map ``mpm`` to all its supported managers."""
-    table = []
-    for mid, m in sorted(pool.items()):
-        line = f"Meta Package Manager,{mid},1"
-        table.append(line)
-
-    output = dedent("""\
-        ```mermaid
-        ---
-        config: {"sankey": {"showValues": false, "width": 800, "height": 400}}
-        ---
-        sankey-beta\n
-        """)
-    output += "\n".join(table)
-    output += "\n```"
-    return output
-
-
-def operation_matrix() -> tuple[str, str]:
-    """Produce a table of managers' metadata and supported operations."""
-    # Build up the column titles.
-    headers = [
-        "Package manager",
-        "Min. version",
-    ]
-
-    # Footnotes are used to details the OSes covered by each platform group.
-    footnotes = []
-
-    for p_obj in MAIN_PLATFORMS:
-        header_title = p_obj.name
-        # Add footnote for groups with more than one platform.
-        if isinstance(p_obj, Group) and len(p_obj) > 1:
-            footnote_tag = f"[^{p_obj.id}]"
-            header_title += footnote_tag
-            platforms_string = ", ".join(
-                sorted(
-                    (
-                        p.name
-                        for p in p_obj.members.values()  # type: ignore[attr-defined]
-                    ),
-                    key=str.casefold,
-                ),
-            )
-            footnotes.append(f"{footnote_tag}: {p_obj.name}: {platforms_string}.")
-        headers.append(header_title)
-
-    headers.extend(f"`{op.name}`" for op in Operations)
-
-    table = []
-    for mid, m in sorted(pool.items()):
-        line = [
-            f"[`{mid}`]({m.homepage_url})"
-            + ("" if not m.deprecated else f" [⚠️]({m.deprecation_url})"),
-            f"{m.requirement}",
-        ]
-        for p_obj in MAIN_PLATFORMS:
-            line.append(
-                p_obj.icon if m.platforms.issuperset(extract_members(p_obj)) else ""
-            )
-        for op in Operations:
-            line.append("✓" if m.implements(op) else "")
-        table.append(line)
-
-    # Set each column alignment.
-    alignments = ["left", "left"]
-    alignments.extend(["center"] * len(MAIN_PLATFORMS))
-    alignments.extend(["center"] * len(Operations))
-
-    rendered_table = render_table(
-        table,
-        headers=headers,
-        table_format=TableFormat.GITHUB,
-        colalign=alignments,
-        disable_numparse=True,
-    )
-
-    return rendered_table, "\n\n".join(footnotes)
-
-
-def replace_content(
-    filepath: Path,
-    new_content: str,
-    start_tag: str,
-    end_tag: str | None = None,
-) -> None:
-    """Replace in a file the content surrounded by the provided start and end tags.
-
-    If no end tag is provided, the whole content found after the start tag will be
-    replaced.
-    """
-    filepath = filepath.resolve()
-    assert filepath.exists(), f"File {filepath} does not exist."
-    assert filepath.is_file(), f"File {filepath} is not a file."
-
-    orig_content = filepath.read_text()
-
-    assert start_tag, "Start tag must be empty."
-    assert start_tag in orig_content, f"Start tag {start_tag!r} not found in content."
-    pre_content, table_start = orig_content.split(start_tag, 1)
-
-    if end_tag:
-        _, post_content = table_start.split(end_tag, 1)
-    else:
-        end_tag = ""
-        post_content = ""
-
-    filepath.write_text(
-        f"{pre_content}{start_tag}{new_content}{end_tag}{post_content}",
-    )
-
-
-def update_readme() -> None:
-    """Update ``readme.md`` with implementation table for each manager we support."""
-    readme = Path(__file__).parent.parent.joinpath("readme.md")
-
-    replace_content(
-        readme,
-        managers_sankey(),
-        "<!-- managers-sankey-start -->\n\n",
-        "\n\n<!-- managers-sankey-end -->",
-    )
-
-    matrix, footnotes = operation_matrix()
-    replace_content(
-        readme,
-        matrix,
-        "<!-- operation-matrix-start -->\n\n",
-        "\n\n<!-- operation-matrix-end -->",
-    )
-    replace_content(
-        readme,
-        footnotes,
-        "<!-- operation-footnotes-start -->\n\n",
-        # mdformat-footnote is stripping all HTML comments after footnotes:
-        # https://github.com/executablebooks/mdformat-footnote/issues/11
-        # So we protect the content to be replaced with an end tag that we put at the
-        # tail of the last footnote line, without any carriage return.
-        "<!-- operation-footnotes-end -->\n",
-    )
--- readme.md
+++ readme.md
@@ -298,6 +298,6 @@ Other subcommands and options are documented in:
 
 [^bsd]: BSD: DragonFly BSD, FreeBSD, MidnightBSD, NetBSD, OpenBSD, SunOS.
 
-[^linux]: Linux: ALT Linux, Amazon Linux, Android, Arch Linux, Buildroot, CachyOS, CentOS, CloudLinux OS, Debian, Exherbo Linux, Fedora, Gentoo Linux, Guix System, IBM PowerKVM, KVM for IBM z Systems, Linux Mint, Mageia, Mandriva Linux, Nobara, openSUSE, openSUSE Tumbleweed, Oracle Linux, Parallels, Pidora, Raspbian, RedHat Enterprise Linux, Rocky Linux, Scientific Linux, Slackware, SUSE Linux Enterprise Server, Tuxedo OS, Ubuntu, Ultramarine, Windows Subsystem for Linux v1, Windows Subsystem for Linux v2, XenServer.
+[^linux]: Linux: Alpine Linux, ALT Linux, Amazon Linux, Android, Arch Linux, Buildroot, CachyOS, CentOS, CloudLinux OS, Debian, Exherbo Linux, Fedora, Gentoo Linux, Guix System, IBM PowerKVM, Kali Linux, KVM for IBM z Systems, Linux Mint, Mageia, Mandriva Linux, Manjaro Linux, Nobara, openSUSE, openSUSE Tumbleweed, OpenWrt, Oracle Linux, Parallels, Pidora, Raspbian, RedHat Enterprise Linux, Rocky Linux, Scientific Linux, Slackware, SUSE Linux Enterprise Server, Tuxedo OS, Ubuntu, Ultramarine, Windows Subsystem for Linux v1, Windows Subsystem for Linux v2, XenServer.
 
 [^unix]: Unix: Cygwin, GNU/Hurd, Haiku, IBM AIX, illumos, Solaris.<!-- operation-footnotes-end -->
